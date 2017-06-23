601ab02112357ffd9452bc84dc57cb45c06dc674
Suhun Kim <cobject@gmail.com>
2017-06-23 01:52:28 -0400

    Add response dummy data

--- tcp-server.js
+++ tcp-server.js
@@ -2,18 +2,29 @@ var net = require('net');
 var fs = require('fs');
 
 var i = 0;
-
+var client = null;
 var server = net.createServer(function(socket) {
 	console.log('server start');
-	socket.setNoDelay(true);
-	socket.on('data', function(data){
-		console.log("data = ", data);
-		socket.write("ok");
-	});
+	client = socket;
+	client.setNoDelay(true);
+	client.on('data', onData);
 });
 
 function onData(data) {
 	console.log('onData()', data);
+	if(data[0] == 1) {
+		if(data[1] == 1) {
+			client.write(Buffer.from([1, 1, 1]));
+		} else if(data[1] == 2) {
+			client.write(Buffer.from([2, 1]));
+		} else if(data[1] == 3) {
+			client.write(Buffer.from([3, 1, 1, 100]));
+		} else if(data[1] == 4) {
+			client.write(Buffer.from([3, 2, 1, 9]));
+		}
+	} else if(data[0] == 2) {
+		client.write(Buffer.from([3, 3, 30]));
+	}
 }
 
 server.listen(3000, '127.0.0.1');
